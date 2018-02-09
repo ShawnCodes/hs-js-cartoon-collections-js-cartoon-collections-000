6a4efcd6f6136389f13fc38da509a3593cb0b353
Shawn T <shawn@huthunt.com>
2018-02-09 16:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function dwarfRollCall(dwarves) {
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarves[i] = dwarves.indexOf(dwarves[i]) + 1 + ". " + dwarves[i] + " ";
 }
+console.log(dwarves.join(''));
+};
 
 function summonCaptainPlanet(planeteerCalls){
 }

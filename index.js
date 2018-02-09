a970a2c378abd86063a3b6a97ca3794f92f5f2b6
Shawn T <shawn@huthunt.com>
2018-02-09 16:50:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function dwarfRollCall(dwarves) {
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarves[i] = dwarves.indexOf(dwarves[i]) + 1 + ". " + dwarves[i] + " ";
+    }
+  return dwarves.join('');
+
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerCalls[i] = planeteerCalls[i].toUpperCase + "!"
+  }
+  return planeteerCalls;
 }
 
-function longPlaneteerCalls(words) {
+
+function longPlaneteerCalls(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > 4) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(array) {
+  for (var i = 0; i < array.length; i++) {
+  if  (array[i] == 'cheddar' || array[i] == 'gouda' || array[i] == 'camembert') {
+      return array[i];
+    }
+    else {
+    continue
+    }
+  }
+  return 'no cheese!';
 }
